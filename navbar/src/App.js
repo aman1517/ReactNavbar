65209c41e89ed3decd1bca3b3347cfb36cf08a68
Aman Verma <vermaman3@gmail.com>
2022-04-10 23:32:43 +0530

    done

---
+++ navbar/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+   <div className="navbar">
+     <div><h5>LOGOBAKERY</h5></div>
+     <div className='flex'>
+       <p>Servies</p>
+       <p>Product</p>
+       <p>Advice</p>
+     </div>
+     <div><button>Constact</button></div>
+   </div>
+  );
+}
+
+export default App;
